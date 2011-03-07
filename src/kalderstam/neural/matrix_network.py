acab6d2b51e22c004d7ae6042445137e5ad94847
Jonas <Jonas@192.168.0.134>
2011-03-07 21:12:36 +0100

    multiprocessing work
    Signed-off-by: Jonas <Jonas@192.168.0.134>

--- src/kalderstam/neural/matrix_network.py
+++ src/kalderstam/neural/matrix_network.py
@@ -3,11 +3,15 @@ from random import uniform
 import numpy
 import logging
 from kalderstam.neural.activation_functions import linear, logsig, tanh
+from kalderstam.util.exceptions import ArgumentError
+from multiprocessing import Pool
 
 logger = logging.getLogger('kalderstam.neural.network')
 
+pool = Pool() #Processing pool, default to number of cpu-cores
+
 def build_feedforward(input_number = 2, hidden_number = 2, output_number = 1, hidden_function = tanh(), output_function = logsig(), random_range=1.0):
-    net = network(input_number = input_number, hidden_number = hidden_number, output_number = output_number, hidden_function = hidden_function, output_function = output_function)
+    net = network(input_number = input_number, hidden_number = hidden_number, output_number = output_number)
     
     #Hidden layer
     for i in range(int(hidden_number)):
@@ -16,7 +20,7 @@ def build_feedforward(input_number = 2, hidden_number = 2, output_number = 1, hi
             weights[input] = uniform(-random_range, random_range)
         #bias
         weights[input_number + i] = uniform(-random_range, random_range)
-        net.set_weights(i, weights)
+        net.set_weights(input_number + i, weights)
         
     #Output nodes
     for i in range(int(output_number)):
@@ -25,21 +29,38 @@ def build_feedforward(input_number = 2, hidden_number = 2, output_number = 1, hi
             weights[hidden] = uniform(-random_range, random_range)
         #bias
         weights[input_number + hidden_number + i] = uniform(-random_range, random_range)
-        net.set_weights(hidden_number + i, weights)
+        net.set_weights(input_number + hidden_number + i, weights)
     
     return net
 
+def pad_input(net, input):
+    if len(input) == len(net):
+        return input
+    else:
+        result = numpy.ones(len(net))
+        for i in range(len(input)):
+            result[i] = input[i]
+        return result
+
 class network:
     
-    def __init__(self, input_number = 2, hidden_number = 2, output_number = 1, hidden_function = linear(), output_function = linear()):
+    def __init__(self, input_number = 2, hidden_number = 2, output_number = 1, activation_functions = None):
         self.num_of_inputs = input_number
         self.num_of_outputs = output_number
         cols = input_number + hidden_number + output_number
-        self.hidden_function = hidden_function
-        self.output_function = output_function
         self.output_start = hidden_number #The row where the first output node resides
         self.weights = numpy.identity(cols)
-        self.weights = numpy.delete(self.weights, range(input_number), 0) #Remove input rows as they will be all zeroes anyway
+        #self.weights = numpy.delete(self.weights, range(input_number), 0) #Remove input rows as they will be all zeroes anyway
+        
+        if activation_functions != None:
+            self.activation_functions = numpy.array(activation_functions)
+        else:
+            t = tanh()
+            l = logsig()
+            self.activation_functions = [None for i in range(input_number)] # Is never used
+            [self.activation_functions.append(t) for i in range(hidden_number)]
+            [self.activation_functions.append(l) for i in range(output_number)]
+            self.activation_functions = numpy.array(self.activation_functions)
             
     def set_weights(self, node, weights):
         self.weights[node] = weights
@@ -48,55 +69,54 @@ class network:
         """Calculates what layers the nodes belong in."""
         #self.layers.append([]) #input nodes are layer 0, are not used for calculations
         distances = [0 for n in range(len(self))] #initialize vector
-        for node_index in range(len(self.weights)):
+        for node_index in range(self.num_of_inputs, len(self.weights)):
             for weight, weight_index in zip(self.weights[node_index], range(len(self.weights[node_index]))):
-                if weight_index != node_index + self.num_of_inputs and weight != 0:
-                    distances[node_index + self.num_of_inputs] = distances[weight_index] + 1
+                if weight_index != node_index and weight != 0:
+                    distances[node_index] = max(distances[node_index], distances[weight_index] + 1)
                     
         self.layers = [[] for d in range(max(distances))]
         for d, weight_index in zip(distances, range(len(distances))):
             if d > 0:
-                self.layers[d-1].append(weight_index - self.num_of_inputs)
+                self.layers[d-1].append(weight_index)
             
     def __len__(self):
         """The length of the network is defined as: input nodes + hidden nodes + output nodes."""
         return len(self.weights[0])
     
-    def __pad_input(self, input):
-        if len(input) == len(self):
-            return input
-        else:
-            result = numpy.ones(len(self))
-            for i in range(len(input)):
-                result[i] = input[i]
-            return result
+    def __check_input(self, input):
+        if len(input) != len(self):
+            raise ArgumentError("Input array was not of correct length: " + str(len(input)) + " vs " + str(len(self)))
     
     def sim(self, input_array):
-        input_array = numpy.array([self.__pad_input(input) for input in input_array])
+        """For a list of input_values, outputs a list of output values. Also modifies input_array so that contains all information as well."""
         return numpy.array([self.update(input) for input in input_array])
             
     #inputs is an array that must match in length with the number of nodes.
     def update(self, inputs):
-        """Returns a numpy array of output value arrays."""
-        col = self.num_of_inputs
-        inputs = self.__pad_input(inputs)
+        """Returns a numpy array of output values. Input vector is also modified! And contains this information as well"""
+        self.__check_input(inputs)
         for rows in self.layers: #Traverse the network
             input_sum = numpy.dot(self.weights[rows], inputs)
-            if row < self.output_start:
-                inputs[rows + self.num_of_inputs] = self.hidden_function.function(input_sum)
-            else:
-                inputs[col] = self.output_function.function(input_sum)
-            col += 1
+            inputs[rows] = [active.function(input) for active, input in zip(self.activation_functions[rows], input_sum)]
+            mp_result = pool.map(mp_evaluate, [(row, self.weights, inputs, self.activation_functions) for row in rows])
         #Now return output values stored in input vector
         return numpy.delete(inputs, range(self.output_start + self.num_of_inputs))
+    
+def mp_evaluate((row, weights, inputs, activation_functions)):
+    """For multiprocessing map, only supports one iterable argument."""
+    input_sum = numpy.dot(weights[row], inputs)
+    return activation_functions[row].function(input_sum)
 
 if __name__ == '__main__': 
     net = build_feedforward(input_number = 2, hidden_number = 2, output_number = 2)
     net.fix_layers()
-    
-    results = net.update([1, 2])
+    i = pad_input(net, [1, 2])
+    results = net.update(i)
+    #print(i)
      
     print(results)
      
-    results2 = net.sim([[1, 2], [2, 3]])
+    i2 = [pad_input(net, [1, 2]), pad_input(net, [2, 3])]
+    results2 = net.sim(i2)
+    print(i2)
     print(results2)
