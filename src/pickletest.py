e9c0521916340ec062b6d372969e6d790360b5f6
Jonas Kalderstam <jonask@thep.lu.se>
2011-03-10 17:45:19 +0100

    What if I only use function object, not the function in the function for activation funtions?

--- src/pickletest.py
+++ src/pickletest.py
@@ -1,5 +1,7 @@
 from multiprocessing import Process
 from multiprocessing.queues import Queue
+from kalderstam.neural.network import build_feedforward
+from kalderstam.neural.matlab_functions import loadsyn1
 
 class Worker(Process):
     def __init__(self, q):
@@ -15,11 +17,30 @@ class Worker(Process):
         element.method()
         element.args_method(*args, **kwargs)
         
+class Neural_Worker(Process):
+    def __init__(self, q):
+        Process.__init__(self)
+        self.queue = q
 
+    def run(self):
+        net, args, kwargs = self.queue.get()
+        Y = net.sim(*args, **kwargs)
+        print Y
+        
+def external_method(what):
+    print(what)
+    
+class class_method():
+    def external_class_method(self, what):
+        print(what)
+    
 class car():
-    def __init__(self):
+    def __init__(self, f = external_method, ext_c = class_method()):
         self.color = "self.attribute works"
         self.motor = engine()
+        self.ext_func = f
+        self.ext_c = ext_c
+        #self.ext_c_func = ext_c.external_class_method
     
     def method(self):
         print("Printing my attribute " + self.color)
@@ -30,6 +51,11 @@ class car():
         
         print("Inner object: " + self.motor.power)
         
+        self.ext_func("Testing method passing..")
+        self.ext_c.external_class_method("Testing class with method passing..")
+        #self.ext_c_func("Testing class method passing..")
+        
+        
     def args_method(self, *args, **kwargs):
         print("Args: " + str(args))
         print("Kwargs: " + str(kwargs))
@@ -37,6 +63,8 @@ class car():
 class engine():
     def __init__(self):
         self.power = "V6 800BHP"
+        
+
 
 
 if __name__ == '__main__':
@@ -49,3 +77,17 @@ if __name__ == '__main__':
     kwargs['one'] = 1
     queue.put((car(), args, kwargs))
     w.join()
+    
+    #Now try with a neural network
+    
+    P1, T1 = loadsyn1(100)
+    P2, T2 = loadsyn1(100)
+                
+    net = build_feedforward(2, 1, 1)
+    
+    nw = Neural_Worker(queue)
+    nw.start()
+    
+    #queue.put((net, [P1], {}))
+    
+    nw.join()
