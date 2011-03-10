6763a71e638febe89d82477fc3cda9063f080126
Jonas Kalderstam <jonask@thep.lu.se>
2011-03-10 21:34:11 +0100

    At around 250 inputs, the 8-core version is faster than single core actually.
    Signed-off-by: Jonas Kalderstam <jonask@thep.lu.se>

--- src/kalderstam/neural/mp_network.py
+++ src/kalderstam/neural/mp_network.py
@@ -1,6 +1,9 @@
 import os
 import numpy
 from multiprocessing import cpu_count, Pool
+from kalderstam.neural.network import build_feedforward
+from kalderstam.neural.matlab_functions import loadsyn1
+from kalderstam.util.decorators import benchmark
 
 def __split_inputs(inputs, number_of_pieces):
     if len(inputs) <= 2*number_of_pieces:
@@ -9,16 +12,22 @@ def __split_inputs(inputs, number_of_pieces):
         return numpy.array_split(inputs, number_of_pieces)
 
 def net_sim((net, args, kwargs)):
-    print('process id:' + str(os.getpid()) + " Starting...")
-    #Y = net.sim(*args, **kwargs)
-    Y = 1
-    print('process id:' + str(os.getpid()) + " Done!")
+    #print('process id:' + str(os.getpid()) + " Starting...")
+    Y = net.sim(*args, **kwargs)
+    #print('process id:' + str(os.getpid()) + " Done!")
     return Y
 
 def mp_net_sim_inputs(net, inputs):
+    """Splits the input into cpu_count pieces, and evaluates the network on each piece in a separate process."""
     cmd_list = [(net, [input_part], {}) for input_part in __split_inputs(inputs, cpu_count())]
-    results = p.map(net_sim, cmd_list)
-    print results
+    pre_results = p.map(net_sim, cmd_list)
+    
+    results = []
+    for result_set in pre_results:
+        for result in result_set:
+            results.append(result)
+    
+    return numpy.array(results)
 
 def mp_nets_sim(nets, inputs):
     pass
@@ -26,4 +35,12 @@ def mp_nets_sim(nets, inputs):
 p = Pool()
 
 if __name__ == '__main__':
-    mp_net_sim_inputs("net", numpy.array([0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]))
+    net = build_feedforward(2, 1, 1)
+    
+    P, T = loadsyn1(250)
+    
+    Y = benchmark(mp_net_sim_inputs)(net, P)
+    
+    benchmark(net.sim)(P)
+    #print Y
+    print len(Y)
