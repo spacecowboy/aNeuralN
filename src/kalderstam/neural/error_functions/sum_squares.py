622e98e309473ad297460f0c8b9cc9488529dc77
Jonas Kalderstam <jonask@thep.lu.se>
2011-04-05 20:34:42 +0200

    Might get overflow errors if result is big compared to target
    Signed-off-by: Jonas Kalderstam <jonask@thep.lu.se>

--- src/kalderstam/neural/error_functions/sum_squares.py
+++ src/kalderstam/neural/error_functions/sum_squares.py
@@ -4,7 +4,11 @@
     ej = target(j) - output(j)."""
 
 def total_error(target, result):
-    return ((target - result) ** 2).sum()/2
+    try:
+        return ((target - result) ** 2).sum() / 2
+    except FloatingPointError as e:
+        print(target, result)
+        raise e
 
 def derivative(target, result):
     """dE/dej = ej = target - result."""
