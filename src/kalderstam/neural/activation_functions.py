2bf021742c1968723cd553b4e1aca60414e065c5
Jonas Kalderstam <jonask@thep.lu.se>
2011-04-05 20:35:18 +0200

    generalized linear() slightly

--- src/kalderstam/neural/activation_functions.py
+++ src/kalderstam/neural/activation_functions.py
@@ -18,15 +18,17 @@ class logsig():
     def derivative(self, x):
         #return exp(x)/((exp(x) + 1)**2)
         y = self.function(x)
-        return y*(1 - y)
+        return y * (1 - y)
 
 class linear():
+    def __init__(self, a = 1):
+        self.a = a
     def __str__(self):
         return 'linear'
     def function(self, x):
-        return x
-    def derivative(self, x):
-        return 1
+        return self.a * x
+    def derivative(self, x): #@UnusedVariable
+        return self.a
 
 class tanh():
     def __str__(self):
@@ -36,4 +38,4 @@ class tanh():
     def derivative(self, x):
         #return (1/math.cosh(x))**2 #sech(x)^2
         y = self.function(x)
-        return (1 - y)*(1 + y)
+        return (1 - y) * (1 + y)
