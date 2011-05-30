56ecaa171ed1b1c42f56036e5ef94d6f1f8c0360
Jonas Kalderstam <jonask@thep.lu.se>
2011-05-30 14:19:18 +0200

    setup.py fully operational!
    Signed-off-by: Jonas Kalderstam <jonask@thep.lu.se>

--- setup.py
+++ setup.py
@@ -5,25 +5,30 @@ import numpy
 
 numpy_include = numpy.get_include()
 
-cox_ext = Extension('neural.error_functions.cox_error_in_c',
+cox_ext = Extension('error_functions.cox_error_in_c',
           sources = ['src/kalderstam/neural/error_functions/cox_error_in_c.c'],
           include_dirs = [numpy_include],
           extra_compile_args = ['-std=c99'])
-network_ext = Extension('neural.fast_network',
+network_ext = Extension('fast_network',
           sources = ['src/kalderstam/neural/fast_network.c', 'src/kalderstam/neural/activation_functions.c'],
           include_dirs = [numpy_include],
           extra_compile_args = ['-std=c99'])
 
 setup(name = 'aNeuralN',
-      version = '1.0',
+      version = '0.2',
       description = 'A neural network package.',
       author = 'Jonas Kalderstam',
       author_email = 'jonas@kalderstam.se',
       url = 'https://github.com/spacecowboy/aNeuralN',
-      packages = ['kalderstam'],
-      package_dir = {'kalderstam': 'src/kalderstam'},
+      packages = ['kalderstam', 'kalderstam.matlab', 'kalderstam.matlab.tests',
+                  'kalderstam.neural', 'kalderstam.neural.tests',
+                  'kalderstam.neural.error_functions', 'kalderstam.neural.error_functions.tests',
+                  'kalderstam.neural.gui', 'kalderstam.neural.training',
+                  'kalderstam.neural.training.tests', 'kalderstam.survival',
+                  'kalderstam.util', 'kalderstam.util.tests'],
+      package_dir = {'': 'src'},
       package_data = {'kalderstam.neural.gui': ['src/kalderstam/neural/gui/*.glade']},
-      ext_package = 'kalderstam',
+      ext_package = 'kalderstam.neural',
       ext_modules = [cox_ext, network_ext],
       requires = ['numpy', 'matplotlib'],
      )
