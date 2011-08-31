13623bfcce3fbe66e72c53eccb49cbb6550c85a5
Jonas Kalderstam <jonask@thep.lu.se>
2011-08-31 11:14:26 +0200

    You my friend, are a complete and utter IDIOT! You shuffle the input and targets DIFFERENTLY? What are you? INSANE?!
    Signed-off-by: Jonas Kalderstam <jonask@thep.lu.se>

--- src/kalderstam/util/filehandling.py
+++ src/kalderstam/util/filehandling.py
@@ -77,15 +77,20 @@ def parse_data(inputs, targetcols = None, inputcols = None, ignorecols = [], ign
 
     return inputs, targets
 
-def print_output(filename, outputs):
+def print_output(outfile, net, filename, targetcols, inputcols, ignorerows, normalize):
+    '''
+    Take a network and a file, outputting the inputs to that network and its output for said input on each line.
+    '''
     inputs = read_data_file(filename)
-    outputs = outputs.tolist()
+    P, T = parse_file(filename, targetcols = targetcols, inputcols = inputcols, ignorerows = ignorerows, normalize = normalize)
+    outputs = net.sim(P).tolist()
     while len(inputs) > len(outputs):
         outputs.insert(0, ["net_output"])
 
     if len(inputs) < len(outputs):
         raise TypeError('Input is smaller than output!')
 
+    lines = []
     for rawline in zip(inputs, outputs):
         line = ''
         for col in rawline[0]:
@@ -94,7 +99,10 @@ def print_output(filename, outputs):
         for col in rawline[1]:
             line += str(col)
 
-        print(line)
+        lines.append(line + '\n')
+
+    with open(outfile, 'w') as f:
+        f.writelines(lines)
 
 def normalizeArray(array):
     '''Returns a new array, will not modify existing array.'''
@@ -164,6 +172,7 @@ def get_validation_set(inputs, targets, validation_size = 0.2, binary_column = N
     validation_targets = numpy.array(validation_targets, dtype = 'float64')
 
     #shuffle the lists
+    BIG FUCKING ERROR HERE. NOTICE HOW YOU SHUFFLE INPUT AND TARGETS DIFFERENTLY? YOU FUCKING IDIOT !
     numpy.random.shuffle(test_inputs)
     numpy.random.shuffle(test_targets)
     numpy.random.shuffle(validation_inputs)
@@ -180,46 +189,54 @@ def get_cross_validation_sets(inputs, targets, pieces, binary_column = None):
     '''
     pieces is the number of validation sets that the data set should be divided into.
     '''
+    totalrows = len(inputs)
+    totalcols = len(inputs[0, :]) + len(targets[0, :])
+
     training_input_sets = [[] for piece in xrange(pieces)]
     training_target_sets = [[] for piece in xrange(pieces)]
     validation_input_sets = [[] for piece in xrange(pieces)]
     validation_target_sets = [[] for piece in xrange(pieces)]
 
-    #if the target has two values, assume one is a binary indicator. we want an equal share of both
-    #matching the diversity of the dataset
-    all = numpy.arange(len(targets))
-    numpy.random.shuffle(all)
-    if binary_column is not None:
-        zeros = all[targets[:, binary_column] == 0]
-        ones = all[targets[:, binary_column] == 1]
+    if (pieces < 2): #No validation set can be divided, return empty set
+        training_input_sets[0].extend(inputs)
+        training_target_sets[0].extend(targets)
     else:
-        zeros = all
-        ones = []
 
-    def divide_sets(indices):
-        sets = numpy.array_split(indices, pieces)
-        k = 0
-        for set in xrange(len(sets)):
-            validation_input_sets[set].extend(inputs[sets[k]])
-            validation_target_sets[set].extend(targets[sets[k]])
-            k += 1
-            k %= pieces
-            for piece in xrange(pieces - 1):
-                training_input_sets[set].extend(inputs[sets[k]])
-                training_target_sets[set].extend(targets[sets[k]])
+        #if the target has two values, assume one is a binary indicator. we want an equal share of both
+        #matching the diversity of the dataset
+        all = numpy.arange(len(targets))
+        numpy.random.shuffle(all)
+        if binary_column is not None:
+            zeros = all[targets[:, binary_column] == 0]
+            ones = all[targets[:, binary_column] == 1]
+        else:
+            zeros = all
+            ones = []
+
+        def divide_sets(indices):
+            sets = numpy.array_split(indices, pieces)
+            k = 0
+            for set in xrange(len(sets)):
+                validation_input_sets[set].extend(inputs[sets[k]])
+                validation_target_sets[set].extend(targets[sets[k]])
                 k += 1
                 k %= pieces
-            #Do one final incrase, to make validation start at +1 next round
-            k += 1
-            k %= pieces
+                for piece in xrange(pieces - 1):
+                    training_input_sets[set].extend(inputs[sets[k]])
+                    training_target_sets[set].extend(targets[sets[k]])
+                    k += 1
+                    k %= pieces
+                #Do one final incrase, to make validation start at +1 next round
+                k += 1
+                k %= pieces
 
-    #First zeros
-    if len(zeros) > 0:
-        divide_sets(zeros)
+        #First zeros
+        if len(zeros) > 0:
+            divide_sets(zeros)
 
-    #Then ones
-    if len(ones) > 0:
-        divide_sets(ones)
+        #Then ones
+        if len(ones) > 0:
+            divide_sets(ones)
 
     #convert types
     for set in xrange(len(training_input_sets)):
@@ -230,6 +247,7 @@ def get_cross_validation_sets(inputs, targets, pieces, binary_column = None):
         validation_target_sets[set] = numpy.asarray(validation_target_sets[set], dtype = 'float64')
 
         #shuffle the lists
+        BIG FUCKING ERROR HERE. NOTICE HOW YOU SHUFFLE INPUT AND TARGETS DIFFERENTLY? YOU FUCKING IDIOT !
         numpy.random.shuffle(training_input_sets[set])
         numpy.random.shuffle(training_target_sets[set])
         numpy.random.shuffle(validation_input_sets[set])
