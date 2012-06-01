44d3bbe000ad51aab841e70c3a2387eeb1a03c01
Jonas Kalderstam <jonask@thep.lu.se>
2012-06-01 13:20:26 +0200

    added train committee to init
    Signed-off-by: Jonas Kalderstam <jonask@thep.lu.se>

--- ann/__init__.py
+++ ann/__init__.py
@@ -2,3 +2,4 @@
 from trainingfunctions.davis_genetic import train_davis_genetic
 from trainingfunctions.genetic import train_evolutionary
 from trainingfunctions.gradientdescent import traingd
+from mp_network import train_committee
